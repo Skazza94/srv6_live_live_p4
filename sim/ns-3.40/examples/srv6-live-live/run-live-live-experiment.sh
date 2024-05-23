4efbff55686cf863b249a1df41870ffc0b8b067d
Mariano Scazzariello <marianoscazzariello@gmail.com>
2024-05-23 10:28:50 +0200

    Minor fix to track "random"

--- sim/ns-3.40/examples/srv6-live-live/run-live-live-experiment.sh
+++ sim/ns-3.40/examples/srv6-live-live/run-live-live-experiment.sh
@@ -10,7 +10,7 @@ active_delay="5us"
 backup_delay="5us"
 congestion_control="TcpCubic"
 ll_flows=1
-active_flows=30
+active_flows=10
 backup_flows=10
 default_buffer="10000p"
 active_buffer="10000p"
@@ -19,10 +19,16 @@ seed=5000
 flow_end=10
 end=40
 dump=""
-random=""
+random="--random"
+
+random_lbl="b"
+if [[ $random != "" ]]
+then
+    random_lbl="r"
+fi
 
 
-result_path="$ll_flows-$active_flows-$backup_flows/$default_bw-$active_bw-$backup_bw/$ll_rate-$active_rate-$backup_rate/$active_delay-$backup_delay/$congestion_control/$default_buffer-$active_buffer-$backup_buffer/$seed"
+result_path="$ll_flows-$active_flows-$backup_flows-$random_lbl/$default_bw-$active_bw-$backup_bw/$ll_rate-$active_rate-$backup_rate/$active_delay-$backup_delay/$congestion_control/$default_buffer-$active_buffer-$backup_buffer/$seed"
 
 mkdir -p results/$result_path
 ../../ns3 run "live-live-experiment --results-path=examples/srv6-live-live/results/$result_path --ll-flows=$ll_flows --active-flows=$active_flows --backup-flows=$backup_flows  --default-bw=$default_bw --ll-rate=$ll_rate --active-bw=$active_bw --active-delay=$active_delay --active-rate=$active_rate --backup-bw=$backup_bw --backup-delay=$backup_delay --backup-rate=$backup_rate --congestion-control=$congestion_control --default-buffer=$default_buffer --active-buffer=$active_buffer --backup-buffer=$backup_buffer --flow-end=$flow_end --end=$end --seed=$seed $random $dump" > results/$result_path/log.txt
