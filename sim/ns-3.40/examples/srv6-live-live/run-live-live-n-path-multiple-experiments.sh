a363dc48acc07c93f90a08e0a5af0590d655878e
Tommaso Caiazzi <tommasocaiazzi@gmail.com>
2024-05-29 18:55:45 +0200

    add n-path multiple experiments

---
+++ sim/ns-3.40/examples/srv6-live-live/run-live-live-n-path-multiple-experiments.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+default_bw="100Mbps"
+path_bw="100Mbps"
+ll_rate="10Mbps"
+maxBytes=12500000
+path_delay="5us"
+congestion_control="TcpCubic"
+ll_flows=1
+default_buffer="10000p"
+path_buffer="10000p"
+seed=1
+end=40
+dump="--dump"
+n_paths=4
+test_type="live-live"
+
+for test_type in "live-live" "no-deduplicate"
+do
+    for n_paths in 2 4 8 16
+    do
+    result_path="n-paths/$test_type/$n_paths/$seed"
+    mkdir -p results/$result_path
+    ../../ns3 run "live-live-n-path --results-path=examples/srv6-live-live/results/$result_path --ll-flows=$ll_flows --default-bw=$default_bw --ll-rate=$ll_rate --path-bw=$path_bw --path-delay=$path_delay --max-bytes=$maxBytes --congestion-control=$congestion_control --default-buffer=$default_buffer --path-buffer=$path_buffer --end=$end --seed=$seed --n-paths=$n_paths --test-type=$test_type $dump" > results/$result_path/log.txt
+
+    python3 flowmon_parser.py results/$result_path/flow-monitor/flow_monitor.xml
+    python3 plot.py results/$result_path/ figures/$result_path
+    done
+done
+chmod 777 -R results
+chmod 777 -R figures
--- sim/ns-3.40/examples/srv6-live-live/run-live-live-n-path.sh
+++ sim/ns-3.40/examples/srv6-live-live/run-live-live-n-path.sh
@@ -15,7 +15,7 @@ dump="--dump"
 n_paths=4
 test_type="live-live"
 
-result_path="$n_paths/$seed"
+result_path="n-paths/$test_type/$n_paths/$seed"
 
 mkdir -p results/$result_path
 ../../ns3 run "live-live-n-path --results-path=examples/srv6-live-live/results/$result_path --ll-flows=$ll_flows --default-bw=$default_bw --ll-rate=$ll_rate --path-bw=$path_bw --path-delay=$path_delay --max-bytes=$maxBytes --congestion-control=$congestion_control --default-buffer=$default_buffer --path-buffer=$path_buffer --end=$end --seed=$seed --n-paths=$n_paths --test-type=$test_type $dump" > results/$result_path/log.txt
